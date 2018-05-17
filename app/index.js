6e23f9ce0414671db27af13152c1f423e218cb3c
Chris Gaona <thebeloved88@sbcglobal.net>
2018-05-16 21:38:30 -0700

    Sets up SignIn layout

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import { SignedOut } from "./router";
+import { SignedOut, SignedIn } from "./router";
 
 export default class App extends React.Component {
   render() {
-    return <SignedOut />;
+    return <SignedIn />;
   }
 }
--- app/router.js
+++ app/router.js
@@ -1,6 +1,5 @@
-import {
-  StackNavigator
-} from "react-navigation";
+import React from 'react';
+import { StackNavigator, TabNavigator } from "react-navigation";
 
 import SignUp from "./screens/SignUp";
 import SignIn from "./screens/SignIn";
@@ -16,6 +15,30 @@ export const SignedOut = StackNavigator({
     screen: SignIn,
     navigationOptions: {
       title: "Sign In"
+    }
+  }
+});
+
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
+
+export const SignedIn = TabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: "Home",
+      // tabBarIcon: ({ tintColor }) => (
+      //   <FontAwesome name="home" size={30} color={tintColor} />
+      // )
+    }
+  },
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: "Profile",
+      // tabBarIcon: ({ tintColor }) => (
+      //   <FontAwesome name="user" size={30} color={tintColor} />
+      // )
     }
   }
 });
